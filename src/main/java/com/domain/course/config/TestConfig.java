75830c101211c051f46e3f92865198774881d082
Hudson Chambarelli <hudson.harca.c@gmail.com>
2023-01-24 12:40:30 -0300

    Payment, one-to-one association

--- src/main/java/com/domain/course/config/TestConfig.java
+++ src/main/java/com/domain/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.domain.course.entities.Category;
 import com.domain.course.entities.Order;
 import com.domain.course.entities.OrderItem;
+import com.domain.course.entities.Payment;
 import com.domain.course.entities.Product;
 import com.domain.course.entities.User;
 import com.domain.course.entitiesnums.OrderStatus;
@@ -81,6 +82,10 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 
 }
